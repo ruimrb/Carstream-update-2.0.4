979c71719ecabdf0c5c866dc049741998d79fc1d
Rui <38188913+ruimrb@users.noreply.github.com>
2022-05-15 13:24:32 +0100

    Try to andd youtube TV

--- carstream-android-auto-82556556e324c6891481043f5638bafb9285610f/app/src/main/java/com/thekirankumar/youtubeauto2/utils/WebviewUtils.java
+++ carstream-android-auto-82556556e324c6891481043f5638bafb9285610f/app/src/main/java/com/thekirankumar/youtubeauto2/utils/WebviewUtils.java
@@ -91,5 +91,8 @@ public class WebviewUtils {
             webView.loadUrl(js);
         }
     }
+    
+    public static final String USER_AGENT = "Mozilla/5.0 (SMART-TV; Linux; Tizen 5.0) AppleWebKit/538.1 (KHTML, like Gecko) Version/5.0 NativeTVAds Safari/538.1";
+        webSettings.setUserAgentString(USER_AGENT);
 
 }
